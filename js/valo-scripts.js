6fdcd9a960eec6f5611fd640cfb253d105a94089
brooklee <brooklee.wilson@gmail.com>
2019-09-09 15:55:52 -0600

    first commit

---
+++ js/valo-scripts.js
@@ -0,0 +1,30 @@
+console.log("hello")
+
+// var form = $('form#test-form'),
+//     url = 'https://script.google.com/macros/s/AKfycbx3vNK9UjmmDV7toGNM9I7J6DuccAYsXlBAbxaj3GoUS-Rup7c/exec'
+
+// 			$('#submit-form').on('click', function(e) {
+// 				e.preventDefault();
+// 				var jqxhr = $.ajax({
+// 					url: url,
+// 					method: "GET",
+// 					dataType: "json",
+// 					data: form.serializeObject()
+// 				}).success(
+// 					console.log("Successs")
+// 				);
+//       })
+      
+      // https://script.google.com/macros/s/AKfycbyI8Xr6LKoFPmV2c1eq6SR-2WDcAcTIBFxjX8XH5BBlKQGOst4/exec
+
+      // const scriptURL = 'https://script.google.com/macros/s/AKfycbyI8Xr6LKoFPmV2c1eq6SR-2WDcAcTIBFxjX8XH5BBlKQGOst4/exec'
+      // const form = document.forms['submit-to-google-sheet']
+    
+      // form.addEventListener('submit', e => {
+      //   e.preventDefault()
+      //   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      //     .then(response => console.log('Success!', response))
+      //     .catch(error => console.error('Error!', error.message))
+      // })
+
+      
